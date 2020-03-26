dbeac6cd0294429f51a9b2adabc78e024d68e00f
karsv <Pbvf2019!>
2020-03-26 18:19:26 +0200

    Initial commit

---
+++ src/main/java/com/example/rickmorty/RickmortyApplication.java
@@ -0,0 +1,13 @@
+package com.example.rickmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickmortyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickmortyApplication.class, args);
+    }
+
+}
